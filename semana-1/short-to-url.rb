d446927f9cb2b50c62de50029d185e775fcc9315
Rogerio Angeliski <rogerio.angeliski@rdstation.com>
2022-05-23 17:09:12 -0300

    improve UX XD

--- semana-1/short-to-url.rb
+++ semana-1/short-to-url.rb
@@ -1,8 +1,18 @@
+prefix = ENV.fetch('APP_WEBSITE','shorter.app')
 
 puts "Digite a url curta que deseja localizar:"
 # Ref https://www.rubyguides.com/2019/10/ruby-chomp-gets/
 shortUrl = gets.chomp
 
 File.foreach("urls.csv") do |line|
-    puts line if (shortUrl.split('shorter.app/')[1] == line.split(',')[0])
+    # Ref https://ruby-doc.com/core/String.html#method-i-split
+    if (shortUrl.split("#{prefix}/")[1] == line.split(',')[0])
+        puts "A sua url original é: #{line.split(',')[1]}" 
+
+        # Ref https://www.rubyguides.com/2019/03/ruby-exit/
+        exit 0
+    end
 end
+
+
+puts "Essa url não tem correspondência no nosso banco de dados"
--- semana-1/url-to-short.rb
+++ semana-1/url-to-short.rb
@@ -1,20 +1,22 @@
+# Ref https://ruby-doc.org/stdlib-3.1.1/libdoc/digest/rdoc/index.html
+require 'digest/sha1'
+
+
+# Ref https://www.rubydoc.info/stdlib/core/ENV
+prefix = ENV.fetch('APP_WEBSITE','shorter.app')
 
 puts "Digite a url que deseja encurtar:"
 # Ref https://www.rubyguides.com/2019/10/ruby-chomp-gets/
 url = gets.chomp
 
-hashUrl = ""
+# Ref https://ruby-doc.org/core-3.1.1/String.html#method-i-5B-5D
+hashUrl = Digest::SHA1.hexdigest(url)[0..7]
+
 #Ref https://www.rubyguides.com/2015/05/working-with-files-ruby/
 # Ref Open mode: https://www.rubydoc.info/stdlib/core/IO:initialize
-File.open("urls.csv", "a+") do |f|
-    # Ref https://ruby-doc.org/stdlib-3.1.1/libdoc/digest/rdoc/index.html
-    
-    require 'digest/sha1'
-
-    # Ref https://ruby-doc.org/core-3.1.1/String.html#method-i-5B-5D
-    hashUrl = Digest::SHA1.hexdigest(url)[0..7]
+File.open("urls.csv", "a+") do |f|    
     f.write("#{hashUrl},#{url}\n")
 end
 
-puts "A sua url curta é: shorter.app/#{hashUrl}"
-
+# ref http://ruby-for-beginners.rubymonstas.org/bonus/string_interpolation.html
+puts "A sua url curta é: #{prefix}/#{hashUrl}"
