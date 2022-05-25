eb74b51b8d5199c0b0eb000c83d1868c97cfab9c
Rogerio Angeliski <rogerio.angeliski@rdstation.com>
2022-05-25 15:57:51 -0300

    first draft - this works

---
+++ semana-1/short-to-url.js
@@ -0,0 +1,38 @@
+const fs = require('fs');
+
+const prefix = 'shorter.app'
+const SHORT_URL_KEY_POSITION = 0
+const ORIGINAL_URL_POSITION = 1
+
+
+// Ref https://nodejs.dev/learn/accept-input-from-the-command-line-in-nodejs
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+
+  
+readline.question('Digite a url curta que deseja localizar:', shortUrl => {
+    readline.close();
+    // Ref https://nodejs.org/api/fs.html#fsreadfilesyncpath-options
+    // Ref https://geshan.com.np/blog/2021/10/nodejs-read-file-line-by-line/#read-file-sync
+    const allFileContents = fs.readFileSync('urls.csv', 'utf-8'); 
+
+    // Ref https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/String/split
+    // Ref https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Array/forEach
+    allFileContents.split(/\r?\n/).forEach(line =>  {
+
+        //Ref https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/String/replace
+        keyFromUser = shortUrl.replace(`${prefix}/`, '')
+        breakedLine = line.split(',')
+        keyFromFile = breakedLine[SHORT_URL_KEY_POSITION]
+
+        if ( keyFromUser == keyFromFile) {
+            console.log(`A sua url original é: ${breakedLine[ORIGINAL_URL_POSITION]}`)
+            process.exit(0)
+        }
+      });
+      
+    console.log("Essa url não tem correspondência no nosso banco de dados")
+    process.exit(1)
+});
---
+++ semana-1/url-to-short.js
@@ -0,0 +1,23 @@
+const crypto = require("crypto")
+const fs = require('fs');
+
+const prefix = 'shorter.app'
+
+// Ref https://nodejs.dev/learn/accept-input-from-the-command-line-in-nodejs
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+
+  
+readline.question('Digite a url que deseja encurtar:', url => {
+    //ref https://www.geeksforgeeks.org/node-js-hash-digest-method/
+    let digest = crypto.createHash("sha256").update(url).digest("hex").substring(0,7)
+
+    //Ref https://nodejs.org/api/fs.html#fswritefilesyncfile-data-options
+    // Ref https://nodejs.org/api/fs.html#file-system-flags
+    fs.writeFileSync("urls.csv", `${digest},${url}`, {'flag':'a'});
+
+    console.log(`A sua url curta é: ${prefix}/${digest}`)
+    readline.close();
+});
